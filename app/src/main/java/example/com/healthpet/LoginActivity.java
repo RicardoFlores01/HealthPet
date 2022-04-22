cc2844a5a60ba193dc81a315ea88c521e0c77d3d
ricardo <ricardo@gmail.com>
2022-04-22 14:17:44 -0500

    Initial commit

---
+++ app/src/main/java/example/com/healthpet/LoginActivity.java
@@ -0,0 +1,14 @@
+package example.com.healthpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/example/com/healthpet/TwoStepVerification.java
@@ -0,0 +1,13 @@
+package example.com.healthpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoStepVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_step_verification);
+    }
+}
